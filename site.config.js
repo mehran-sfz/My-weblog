7b2a2fc1c85bb7815d93e9c5509c171c52c09828
Mehran safarzadeh <32435304+mehran-sfz@users.noreply.github.com>
2024-12-22 15:56:02 +0330

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,26 +1,26 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Mehran Safarzadeh",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Just Developer",
+    bio: "I develop everything that comes to my mind.",
+    email: "mehran.sfz99@gmail.com",
+    linkedin: "mehran-safarzadeh",
+    github: "mehran-sfz",
+    instagram: "mehransafarzadeh",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Personal Blog`,
+      href: "https://github.com/mehran-sfz",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "Personal Blog",
+    description: "welcome to my daily life!",
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
